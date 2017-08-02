9d078455530ecdda7a2ca8427270fa24973b9959
Farhan Faruque <russel.cse05@gmail.com>
2017-08-02 16:30:43 +0600

    added some codes for jwt

--- src/main/java/me/farhan/jwtboot/model/dto/UserDto.java
+++ src/main/java/me/farhan/jwtboot/model/dto/UserDto.java
@@ -1,14 +1,59 @@
 package me.farhan.jwtboot.model.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Value
-public class UserDto {
+public class UserDto implements UserDetails {
 
   private final String firstName;
   private final String lastName;
   private final String userName;
+  private final String password;
   private final String email;
   private final boolean isEnambled;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @JsonIgnore
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return userName;
+  }
+
+  @JsonIgnore
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @JsonIgnore
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @JsonIgnore
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
 }
--- src/main/java/me/farhan/jwtboot/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/me/farhan/jwtboot/security/JwtAuthenticationTokenFilter.java
@@ -1,22 +1,34 @@
 package me.farhan.jwtboot.security;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtAuthenticationTokenFilter implements Filter {
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+  private final Log logger = LogFactory.getLog(this.getClass());
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Autowired
+  private JwtTokenUtil jwtTokenUtil;
+
+  @Value("${jwt.header}")
+  private String tokenHeader;
 
   @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-
-  }
-
-  @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-  }
-
-  @Override
-  public void destroy() {
+  protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                  HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
   }
 }
